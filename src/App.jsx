9fd30c2e64e41adeee9d8981eebf5b767be8d0b0
hamza1851 <hamza.dev011@gmail.com>
2024-10-19 23:12:19 +0530

    Add Edit functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import JobsPage from "./pages/JobsPage"
 import NotFoundPage from "./pages/NotFoundPage"
 import JobPageSingular, { jobLoader } from "./pages/JobPageSingular"
 import AddJobPage from "./pages/AddJobPage"
+import EditJobPage from "./pages/EditJobPage"
 
 const App = () => {
   // Add new Job
@@ -32,6 +33,18 @@ const App = () => {
     return
   }
 
+  // Update Job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(job),
+    })
+    return
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
@@ -39,6 +52,11 @@ const App = () => {
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
 
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
         <Route
           path="/jobs/:id"
           element={<JobPageSingular deleteJob={deleteJob} />}

998501776ebfcdfb348098fb146a7c385023084b
hamza1851 <hamza.dev011@gmail.com>
2024-10-19 10:39:45 +0530

    Add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { useState } from "react"
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import HomeCards from "./components/HomeCards"
 
 function App() {
-  return <></>
+  return (
+    <>
+      <Navbar />
+      <Hero title="Find the React job that fits your skills and needs" />
+      <HomeCards />
+    </>
+  )
 }
 
 export default App
